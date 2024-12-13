96a34a251560794eab16628929d1f52cfd2f784b
sirJosef <ysfhcb@gmail.com>
2024-12-13 09:48:15 +0300

    I think the date is done if u need any getters and setters let me now

--- Branch.java
+++ Branch.java
@@ -1,3 +1,32 @@
-public class Branch {
+import java.awt.print.Book;
+import java.util.ArrayList;
 
+public class Branch {
+    private String branchName;
+    private ArrayList<Book> books=new ArrayList<Book>();
+    private double balance;
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
